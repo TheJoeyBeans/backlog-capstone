94d3afa4584185823d9529784d4e54bee0ea0872
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-12-03 09:23:27 -0700

    frontEnd created and up

---
+++ backlog-frontend/src/HomePage/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+	constructor(){
+		super();	
+	}
+
+	render(){
+		return(
+			<div>
+				<p>Hey</p>
+			</div>
+		)
+	}
+}
+
+export default HomePage;
