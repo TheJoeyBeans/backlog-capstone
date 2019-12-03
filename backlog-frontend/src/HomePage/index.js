424d7989efff0e56cf597a9a2a25282d85e10bf6
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-12-03 14:09:01 -0700

    API fetch is getting data and storing it properly in state

--- backlog-frontend/src/HomePage/index.js
+++ backlog-frontend/src/HomePage/index.js
@@ -6,16 +6,36 @@ class HomePage extends Component {
 	constructor(){
 		super();
 
+		this.state = {
+			foundGames: [],
+			showGameResults: false
+		}
 	}
 
 	fetchGameResults = (query) => {
-		const searchUrl = `https://api.rawg.io/api/games?search=${query}&page_size=5`;
+		const searchUrl = `https://api.rawg.io/api/games?search=${query}&page_size=10`;
 		axios.get(searchUrl, {
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		}).then(response =>{
-			console.log(response)
+			const foundResults = response.data.results;
+			for(let i = 1; i < foundResults.length; i++){
+				const gameTitle = foundResults[i].name;
+				const gamePic = foundResults[i].background_image;
+				const gameId = foundResults[i].id;
+
+				this.setState(state=>{
+					const foundGames = state.foundGames.concat({
+						title: gameTitle,
+						pic: gamePic,
+						id: gameId
+					});
+					return{
+						foundGames
+					}
+				})
+			}
 		})
 		this.setState({
 			searchQuery: ''
