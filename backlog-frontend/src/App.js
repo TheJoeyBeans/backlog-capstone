ac654017c0d937493ef4d13ac8d0e9d6dd92fb71
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-12-03 13:43:33 -0700

    Search bar is returning back data from API

--- backlog-frontend/src/App.js
+++ backlog-frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HomePage from './HomePage';
-import SiteHeader from './SiteHeader';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
--- backlog-frontend/src/HomePage/index.js
+++ backlog-frontend/src/HomePage/index.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import SiteHeader from '../SiteHeader';
+import axios from 'axios';
 
 class HomePage extends Component {
 	constructor(){
-		super();	
+		super();
+
 	}
 
+	fetchGameResults = (query) => {
+		const searchUrl = `https://api.rawg.io/api/games?search=${query}&page_size=5`;
+		axios.get(searchUrl, {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(response =>{
+			console.log(response)
+		})
+		this.setState({
+			searchQuery: ''
+		})
+	}
 	render(){
 		return(
 			<div>
-				<SiteHeader />
+				<SiteHeader search={this.handleQuery} fetchResults={this.fetchGameResults}/>
 				<p>Hey</p>
 			</div>
 		)
