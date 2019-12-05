18b03a85242d558cbafb47c44ec94c097740d834
Joseph Benbella <47164559+TheJoeyBeans@users.noreply.github.com>
2019-12-05 13:30:13 -0700

    added styling to registration page

--- backlog-frontend/src/RegistrationPage/index.js
+++ backlog-frontend/src/RegistrationPage/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class RegistrationPage extends Component {
 	constructor() {
@@ -21,7 +22,8 @@ class RegistrationPage extends Component {
 
 	handleSubmit = async (e) => {
 		e.preventDefault();
-		const registrationUrl = `${process.env.REACT_APP_BACKEND_URL}/register`;
+		const registrationUrl = `${process.env.REACT_APP_BACKEND_URL}/register/`;
+		console.log(registrationUrl, "This is the registration URL"); 
 		const registerResponse = await fetch(registrationUrl, {
 			method: 'POST',
 			body: JSON.stringify(this.state),
@@ -30,28 +32,36 @@ class RegistrationPage extends Component {
 				'Content-Type': 'application/json'
 			}
 		});
-
+		console.log(registerResponse, "This is the registration response")
 		const parsedResponse = await registerResponse.json();
 		console.log(parsedResponse, "this is your parsedResponse");
+		this.props.history.push('/');
 	}
 
 	render() {
 		return(
-			<Form>
-				<Form.Group>
-					<Form.Label>Display Name</Form.Label>
-					<Form.Control name='displayName' onChange={this.handleChange} type='text' placeholder='Enter Display Name'/>
-				</Form.Group>
-				<Form.Group>
-					<Form.Label>Email address</Form.Label>
-					<Form.Control name='email' onChange={this.handleChange} type='email' placeholder='Enter email'/>
-				</Form.Group>
-				<Form.Group>
-					<Form.Label>Password</Form.Label>
-					<Form.Control name='password' onChange={this.handleChange}  type='password' placeholder='Password'/>
-				</Form.Group>
-				<Button onClick={this.handleSubmit}variant='primary' type='submit'>Submit</Button>
-			</Form>
+			<div>
+				<Navbar sticky="top" variant="dark" bg='dark'>	
+					<Link to="/"><Navbar.Brand>Backlog</Navbar.Brand></Link>		
+				</Navbar>
+				<Container className="registrationContainer">
+					<Form>
+						<Form.Group>
+							<Form.Label>Display Name</Form.Label>
+							<Form.Control name='displayName' onChange={this.handleChange} type='text' placeholder='Enter Display Name'/>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>Email address</Form.Label>
+							<Form.Control name='email' onChange={this.handleChange} type='email' placeholder='Enter email'/>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>Password</Form.Label>
+							<Form.Control name='password' onChange={this.handleChange}  type='password' placeholder='Password'/>
+						</Form.Group>
+						<Button onClick={this.handleSubmit}variant='primary' type='submit'>Submit</Button>
+					</Form>
+				</Container>
+			</div>
 		)
 	}
 }
